2d9d2131cb1f31c635f4c8bfeb767833a47afcd6
koenverburg <koenverburg@users.noreply.github.com>
2022-11-20 13:44:24 +0000

    deploy: 858359f06dca8e948e76f3f2935624783abedf82

---
+++ _next/static/FIxr09qVcSgbWXwHkBYBU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6efc2594e3266251.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
