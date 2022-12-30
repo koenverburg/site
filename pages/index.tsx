1f30e5f82fc5ec23aec3eb979cff23f9206fab5f
Koen Verburg <creativekoen@gmail.com>
2022-12-30 15:34:26 +0100

    feat: new avatar

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,13 @@ const Bold = (props) => {
 const IndexPage = () => {
   const Twitch = link('Twitch', 'https://www.twitch.tv/conradtheprogrammer')
   const Instagram = link('Instagram', 'https://www.instagram.com/conradtheprogrammer/')
+  const Blog = link('Blog', 'https://conradtheprogrammer.medium.com')
+  const Adidas = link('Adidas', 'https://adidas.com')
+  const AdidasCom = link('adidas.com', 'https://adidas.com')
+
+  const Twitter = link('Twitter', 'https://twitter.com/koenverburg')
+  const Call = link('call', 'https://appointments.lokiapp.live/consultant/WUJFxrwyO0MMZj6F9lEKOsVFtpx2')
+
   return (
     <Layout title="Conrad The Programmer">
       <div className="text-center mb-4">
@@ -23,19 +30,25 @@ const IndexPage = () => {
           width="460"
           height="460"
           alt="Avatar"
-          src="/assets/avatar-github.jpg"
+          src="/assets/new-avatar.jpg"
           className="rounded-full w-32 mb-4 mx-auto"
         />
         <h1 className="font-bold text-3xl tracking-tight">Koen Verburg</h1>
         <h2 className="text-gray-500">
-          Software Engineer at <Bold>Adidas</Bold>
+          Software Engineer at <Adidas />
         </h2>
         <h5 className="text-gray-500 mb-2">On Social Media as <Bold>Conrad The Programmer</Bold></h5>
       </div>
 
-      <article className="mb-4">
-        I care about Developer Experience, CI/CD, and faster roll-outs. During the day I work on the ReactJS app that powers adidas.com
-        At night I stream on <Twitch />, work on posts for <Instagram />, or one of my Saas projects 
+      <article className="mb-4 text-center">
+        I care about Developer Experience which includes a fast CI/CD, fast feedback loop during active development, repeatable and solid infrastructure using IaC, Docker and kubernetes.
+        <br /><br />During the day I work on the ReactJS app that powers <AdidasCom />{' '}
+        <br />At night I stream on <Twitch />, work on content for <Instagram /> or my <Blog />, or one of my SaaS projects. 
+      </article>
+
+      <article className="mb-4 text-center">
+        Want you Development Team to move to production multiple times a day instead once ever two weeks?
+        <br />Book a <Call /> or DM me on <Twitter /> or <Instagram />
       </article>
 
     </Layout>
