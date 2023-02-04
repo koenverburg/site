e79c4e857455f329316bfb7ce9f3324e5a789d16
Koen Verburg <creativekoen@gmail.com>
2023-02-04 09:20:11 +0100

    Revert "feat: init umami tracking"
    This reverts commit 1034323e03da3823aa1dd2042d553c81bb12374f.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,4 @@
-'use client';
-
-import * as React from 'react'
 import "../styles/global.css";
-import Script from 'next/script'
 
 export default function RootLayout({
   children,
@@ -11,15 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head>
-        <Script
-          async
-          defer
-          src={process.env.NEXT_PUBLIC_UMAMI_DATA_DOMAIN}
-          data-domain={process.env.NEXT_PUBLIC_UMAMI_URL}
-          data-website-id={process.env.NEXT_PUBLIC_UMAMI_SITE_ID}
-        />
-      </head>
+      <head />
       <body>{children}</body>
     </html>
   );
