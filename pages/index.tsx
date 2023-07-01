6ec3aefe10aa49b4ce7203ba599771389626ed7f
Koen Verburg <creativekoen@gmail.com>
2023-07-01 18:39:01 +0200

    chore: added plausible tracking

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,61 @@
-import Image from 'next/image'
-import {Layout} from '../components/Layout'
+import Image from "next/image";
+import { Layout } from "../components/Layout";
+import Plausible from "plausible-tracker";
+
+const { trackEvent } = Plausible({
+  trackLocalhost: true,
+  domain: 'conradtheprogrammer.com'
+});
+
+const handleClick = (label: string, url: string) => () => {
+  trackEvent(
+    "click",
+    {
+      props: {
+        label,
+      },
+    },
+    {
+      url,
+    }
+  );
+
+  window.open(url, "_blank");
+};
 
 /* eslint-disable react/display-name */
 const link = (label: string, link: string) => (): JSX.Element => {
-  const eventname = label.toLowerCase().replaceAll('.', '-').replaceAll(' ', '-')
+  const eventname = label
+    .toLowerCase()
+    .replaceAll(".", "-")
+    .replaceAll(" ", "-");
+
   return (
     <a
       href={link}
       target="_blank"
       rel="noreferrer"
+      onClick={handleClick(eventname, link)}
       className={`underline font-semibold text-orange-500 umami--click--${eventname}`}
-    >{label}</a>
-  ) 
-}
+    >
+      {label}
+    </a>
+  );
+};
 
 const Bold = (props: { children: React.ReactNode }) => {
-  return <span className="font-semibold text-blue-500">{props.children}</span>
-}
+  return <span className="font-semibold text-blue-500">{props.children}</span>;
+};
 
 const IndexPage = () => {
-  const Twitch = link('Twitch', 'https://www.twitch.tv/conradtheprogrammer')
-  const Instagram = link('Instagram', 'https://www.instagram.com/conradtheprogrammer/')
-  const Blog = link('Blog', 'https://conradtheprogrammer.medium.com')
-  const Adidas = link('Adidas', 'https://adidas.com')
-  const AdidasCom = link('adidas.com', 'https://adidas.com')
+  const Twitch = link("Twitch", "https://www.twitch.tv/conradtheprogrammer");
+  const Instagram = link(
+    "Instagram",
+    "https://www.instagram.com/conradtheprogrammer/"
+  );
+  const Blog = link("Blog", "https://conradtheprogrammer.medium.com");
+  const Adidas = link("Adidas", "https://adidas.com");
+  const AdidasCom = link("adidas.com", "https://adidas.com");
 
   // const Twitter = link('Twitter', 'https://twitter.com/koenverburg')
   // const Call = link('call', 'https://appointments.lokiapp.live/consultant/WUJFxrwyO0MMZj6F9lEKOsVFtpx2')
@@ -44,13 +76,21 @@ const IndexPage = () => {
         <h2 className="text-gray-500">
           Software Engineer at <Adidas />
         </h2>
-        <h5 className="text-gray-500 mb-2">On Social Media as <Bold>Conrad The Programmer</Bold></h5>
+        <h5 className="text-gray-500 mb-2">
+          On Social Media as <Bold>Conrad The Programmer</Bold>
+        </h5>
       </div>
 
       <article className="mb-4 text-center">
-        I care about Developer Experience which includes a fast CI/CD, fast feedback loop during active development, repeatable and solid infrastructure using IaC, Docker and kubernetes.
-        <br /><br />During the day I work on the ReactJS app that powers <AdidasCom />{' '}
-        <br />At night I stream on <Twitch />, work on content for <Instagram /> or my <Blog />, or one of my SaaS projects. 
+        I care about Developer Experience which includes a fast CI/CD, fast
+        feedback loop during active development, repeatable and solid
+        infrastructure using IaC, Docker and kubernetes.
+        <br />
+        <br />
+        During the day I work on the ReactJS app that powers <AdidasCom />{" "}
+        <br />
+        At night I stream on <Twitch />, work on content for <Instagram /> or my{" "}
+        <Blog />, or one of my SaaS projects.
       </article>
 
       {/*
@@ -59,9 +99,8 @@ const IndexPage = () => {
         <br />Book a <Call /> or DM me on <Twitter /> or <Instagram />
       </article>
       */}
-
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
